e6018ecd180f2b8c2bcff0337469c6eb002f3e59
Anurat Eiamphoklarp <rhymerealm@gmail.com>
2025-10-02 23:25:50 +0700

    adjust nav / add footer

--- people-constitution-prototype/app.js
+++ people-constitution-prototype/app.js
@@ -1,11 +1,11 @@
 function toggleMenu() {
-  const nav = document.getElementById('nav-menu');
-  const menu = document.getElementById('menu-list');
+  const nav = document.getElementById('nav');
+  const menu = document.getElementById('menu');
   nav.classList.toggle('show');
   menu.classList.toggle('show');
 }
 
 (function() {
-  const nav = document.getElementById('nav-menu');
+  const nav = document.getElementById('nav');
   nav.addEventListener('click', toggleMenu);
 })();
